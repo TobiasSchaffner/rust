395b87cb9b17e75f49448759f73f688b9ac3f5ea
Tobias Schaffner <tschaff@genua.de>
2017-08-11 15:39:32 +0200

    Add relative l4re library positions.
    By now the root of the l4re libraries can be passed with
    a environment variable. This will be changes later.
    
    As alloc_system in target is deprecated. Change
    exe_allocation_crate to None.

--- src/librustc_back/target/l4re_base.rs
+++ src/librustc_back/target/l4re_base.rs
@@ -12,20 +12,58 @@ use PanicStrategy;
 use LinkerFlavor;
 use target::{LinkArgs, TargetOptions};
 use std::default::Default;
+use std::ffi::OsString;
+use std::env;
 
 pub fn opts() -> TargetOptions {
+    let l4re_lib_path = env::var_os("L4RE_LIB_PATH_RUST").unwrap_or(OsString::new()).into_string().unwrap();
     let mut pre_link_args = LinkArgs::new();
     pre_link_args.insert(LinkerFlavor::Ld, vec![
-            "-nostdlib".to_string(),
+            format!("-T{}/main_stat.ld", l4re_lib_path),
+            "--defsym=__executable_start=0x01000000".to_string(),
+            "--defsym=__L4_KIP_ADDR__=0x6ffff000".to_string(),
+            format!("{}/crt1.o", l4re_lib_path),
+            format!("{}/crti.o", l4re_lib_path),
+            format!("{}/crtbeginT.o", l4re_lib_path),
+    ]);
+    let mut post_link_args = LinkArgs::new();
+    post_link_args.insert(LinkerFlavor::Ld, vec![
+            format!("{}/l4f/libpthread.a", l4re_lib_path),
+            format!("{}/l4f/libc_be_sig.a", l4re_lib_path),
+            format!("{}/l4f/libc_be_sig_noop.a", l4re_lib_path),
+            format!("{}/l4f/libc_be_socket_noop.a", l4re_lib_path),
+            format!("{}/l4f/libc_be_fs_noop.a", l4re_lib_path),
+            format!("{}/l4f/libc_be_sem_noop.a", l4re_lib_path),
+            format!("{}/l4f/libl4re-vfs.o.a", l4re_lib_path),
+            format!("{}/l4f/lib4re.a", l4re_lib_path),
+            format!("{}/l4f/lib4re-util.a", l4re_lib_path),
+            format!("{}/l4f/libc_support_misc.a", l4re_lib_path),
+            format!("{}/l4f/libsupc++.a", l4re_lib_path),
+            format!("{}/l4f/lib4shmc.a", l4re_lib_path),
+            format!("{}/l4f/lib4re-c.a", l4re_lib_path),
+            format!("{}/l4f/lib4re-c-util.a", l4re_lib_path),
+            format!("{}/l4f/libgcc_eh.a", l4re_lib_path),
+            format!("{}/l4f/libdl.a", l4re_lib_path),
+            "--start-group".to_string(),
+            format!("{}/l4f/libl4util.a", l4re_lib_path),
+            format!("{}/l4f/libc_be_l4re.a", l4re_lib_path),
+            format!("{}/l4f/libuc_c.a", l4re_lib_path),
+            format!("{}/l4f/libc_be_l4refile.a", l4re_lib_path),
+            "--end-group".to_string(),
+            format!("{}/l4f/libl4sys.a", l4re_lib_path),
+            "-gc-sections".to_string(),
+            format!("{}/crtend.o", l4re_lib_path),
+            format!("{}/crtn.o", l4re_lib_path),
     ]);
 
     TargetOptions {
         executables: true,
         has_elf_tls: false,
-        exe_allocation_crate: Some("alloc_system".to_string()),
+        exe_allocation_crate: None,
         panic_strategy: PanicStrategy::Abort,
         linker: "ld".to_string(),
         pre_link_args: pre_link_args,
+        post_link_args: post_link_args,
         target_family: Some("unix".to_string()),
         .. Default::default()
     }
