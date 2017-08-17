b49858f3a470b17a93fc8f3a01bfafe08c6bbadd
Tobias Schaffner <tschaff@genua.de>
2017-08-17 12:58:13 +0200

    Change min stacksize in util

--- src/libstd/sys_common/util.rs
+++ src/libstd/sys_common/util.rs
@@ -22,7 +22,12 @@ pub fn min_stack() -> usize {
         n => return n - 1,
     }
     let amt = env::var("RUST_MIN_STACK").ok().and_then(|s| s.parse().ok());
+
+    #[cfg(not(target_os = "l4re"))]
     let amt = amt.unwrap_or(2 * 1024 * 1024);
+    #[cfg(target_os = "l4re")]
+    let amt = amt.unwrap_or(1 * 1024 * 1024);
+
     // 0 is our sentinel value, so ensure that we'll never see 0 after
     // initialization has run
     MIN.store(amt + 1, Ordering::SeqCst);
