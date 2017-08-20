6794906a73a735af9e660ca5c003ee9245ae5724
Sebastian Humenda <shumenda@gmx.de>
2017-08-20 11:29:36 +0200

    Strip whitespace from GCC output more reliably

--- src/librustc_back/target/l4re_base.rs
+++ src/librustc_back/target/l4re_base.rs
@@ -17,14 +17,13 @@ use std::process::Command;
 
 // Use GCC to locate code for crt* libraries from the host, not from L4Re. Note
 // that a few files also come from L4Re, for these, the function shouldn't be
-// used.
+// used. This uses GCC for the location of the file, but GCC is required for L4Re anyway.
 fn get_path_or(filename: &str) -> String {
     let child = Command::new("gcc")
             .arg(format!("-print-file-name={}", filename)).output()
             .expect("Failed to execute GCC");
-    let mut path = String::from_utf8(child.stdout).unwrap();
-    path.pop(); // remove trailing newline
-    path
+    String::from_utf8(child.stdout)
+        .expect("Couldn't read path from GCC").trim().into()
 }
 
 pub fn opts() -> TargetOptions {
